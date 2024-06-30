0641dec4a804c9542e2e04e51721d83370578a36
David Valic <david.valic@beatgridmedia.com>
2024-07-01 00:50:50 +0200

    Add all subjects

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -20,28 +21,40 @@ selected_features = [
     'gyroscope_z_axis_rotation_rate'
 ]
 
-subject = "sg"
-data_path = "data/" + subject
-model_file_name = 'models/tcn_model_{}.pth'.format(subject)
+subjects = [
+    "as",
+    "av",
+    "bl",
+    "cv",
+    "kh",
+    "sg"
+]
+
+data_path = "data/"
+model_file_name = 'models/tcn_model_{}.pth'.format("".join(subjects))
 
 
-def read_data(directory):
+def read_data(subjects, directory):
     data_read = []
-    for item in os.listdir(directory):
-        if os.path.isdir(os.path.join(directory, item)):
-            if item != "other" and not item.startswith("0"):
-                trip_id = item.split('_')[0]
-                data_read.append(TripData(trip_id, item, data_path))
+    for subject in subjects:
+        path = directory + subject
+        for item in os.listdir(path):
+            if os.path.isdir(os.path.join(path, item)):
+                if item != "other" and not item.startswith("0"):
+                    trip_id = item.split('_')[0]
+                    data_read.append(TripData(trip_id, item, path))
     return data_read
 
 
-def read_test_data(directory):
+def read_test_data(subjects, directory):
     data_read = []
-    for item in os.listdir(directory):
-        if os.path.isdir(os.path.join(directory, item)):
-            if item != "other":
-                trip_id = item.split('_')[0]
-                data_read.append(TripData(trip_id, item, data_path))
+    for subject in subjects:
+        path = directory + subject
+        for item in os.listdir(path):
+            if os.path.isdir(os.path.join(path, item)):
+                if item != "other":
+                    trip_id = item.split('_')[0]
+                    data_read.append(TripData(trip_id, item, path))
     return data_read
 
 
@@ -52,7 +65,7 @@ output_size = 1
 
 
 def train():
-    data = read_data(data_path)
+    data = read_data(subjects, data_path)
     dataset = DrivingDataset(data, selected_features)
     dataloader = DataLoader(dataset, batch_size=4, collate_fn=collate_fn, shuffle=True)
 
@@ -76,8 +89,9 @@ def train():
     # Save the trained model
     torch.save(model.state_dict(), model_file_name)
 
+
 def test():
-    test_data = read_test_data(data_path)
+    test_data = read_test_data(subjects, data_path)
     test_dataset = DrivingDataset(test_data, selected_features)
     test_dataloader = DataLoader(test_dataset, batch_size=1, collate_fn=collate_fn)
 
@@ -93,16 +107,39 @@ def test():
 
     with torch.no_grad():
         index = 0
+        predicted_scores = []
+        actual_scores = []
         for batch_X, batch_y, lengths in test_dataloader:
             batch_X = batch_X.permute(0, 2, 1)  # Change to (batch_size, num_features, seq_length)
             outputs = loaded_model(batch_X, lengths)
 
             # Assuming your model outputs a single value for the comfort score prediction
-            predicted_score = outputs.item()
+            predicted_scores.append(outputs.item())
 
-            actual_score = test_data[index].get_comfort_score()
-            print("Predicted comfort score: {} - actual score {}".format(predicted_score, actual_score))
+            actual_scores.append(test_data[index].get_comfort_score())
             index += 1
+        index = 0
+        for predicted in predicted_scores:
+            print("Predicted comfort score: {} - actual score {}".format(predicted, actual_scores[index]))
+            index += 1
+        # calculate different evaluation metrics
+        #MAE
+        predicted = np.array(predicted_scores).astype(np.float64)
+        actual = np.array(actual_scores).astype(np.float64)
+        mae = np.mean(np.abs(predicted - actual))
+        # MSE
+        mse = np.mean((predicted - actual) ** 2)
+        # RMSE
+        rmse = np.sqrt(mse)
+        # R-squared
+        ss_res = np.sum((actual - predicted) ** 2)
+        ss_tot = np.sum((actual - np.mean(actual)) ** 2)
+        r_squared = 1 - (ss_res / ss_tot)
+        # MAPE
+        mape = np.mean(np.abs((actual - predicted) / actual)) * 100
+
+        print("MAE: {}, MSE: {}, RMSE: {}, R-SQUARED: {}, MAPE: {}".format(mae, mse, rmse, r_squared, mape))
+
 
 
 if __name__ == '__main__':
--- trip_data.py
+++ trip_data.py
@@ -6,8 +6,39 @@ from other.data_plotter import DataPlotter
 
 DELIMETER = ";"
 
+columns_to_average = [
+    # "heart_rate_bpm",
+    "accelerometer_x_axis_acceleration",
+    "accelerometer_y_axis_acceleration",
+    "accelerometer_z_axis_acceleration",
+    "gravity_x_axis_gravity",
+    "gravity_y_axis_gravity",
+    "gravity_z_axis_gravity",
+    "gravity_accuracy",
+    "gyroscope_x_axis_rotation_rate",
+    "gyroscope_y_axis_rotation_rate",
+    "gyroscope_z_axis_rotation_rate",
+    "gyroscope_accuracy",
+    "gyroscope_orientation_x", "gyroscope_orientation_y", "gyroscope_orientation_z",
+    "linear_accelerometer_x_axis_linear_acceleration", "linear_accelerometer_y_axis_linear_acceleration",
+    "linear_accelerometer_z_axis_linear_acceleration", "linear_acceleration_accuracy",
+    "rotation_vector_x_axis_rotation_vector", "rotation_vector_y_axis_rotation_vector",
+    "rotation_vector_z_axis_rotation_vector", "rotation_vector_scalar", "rotation_vector_accuracy",
+    "rotation_vector_orientation_x", "rotation_vector_orientation_y", "rotation_vector_orientation_z"
+]
+
 
 class TripData:
+
+    def data_with_calibration(self):
+        calibrated_data = self.data
+        trimmed_calibration_data = self.calibration_data.iloc[100:-100]
+        for key, calibration in trimmed_calibration_data.items():
+            if key in columns_to_average:
+                calibrated_data[key] = self.data[key] - calibration.mean()
+                # calibrated_data[key] = self.data[key]
+        return calibrated_data
+
     def __init__(self, trip_id, filename, path):
         self.trip_id = trip_id
         self.filename = filename
@@ -30,11 +61,24 @@ class TripData:
         self.pre_panas_evaluated = PanasEvaluation(self.pre_panas_data.values)
         self.post_panas_evaluated = PanasEvaluation(self.post_panas_data.values)
 
+        # Data with removed average from calibration
+        self.data_with_calibration = self.data
+        trimmed_calibration_data = self.calibration_data.iloc[100:-100]
+        for key, calibration in trimmed_calibration_data.items():
+            if key in columns_to_average:
+                self.data_with_calibration[key] = self.data[key] - calibration.mean()
+
     def get_trip_id(self):
         return self.trip_id
 
     def get_data(self):
         return self.data
+
+    def get_calibration_data(self):
+        return self.calibration_data
+
+    def get_data_with_calibration(self):
+        return self.data_with_calibration
 
     def get_comfort_score(self):
         return self.post_specific["answer"][0]
